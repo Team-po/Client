b678a84e4cf8243aa424bcebf1ea23e6550e18a0
daeun <jaeunda@gmail.com>
2026-04-27 00:35:25 +0900

    feat(presentation): add team-po eighth deck #28

---
+++ src/pages/team-po-presentation-eighth-page.tsx
@@ -0,0 +1,5 @@
+import { TeamPoPresentationEighthView } from "@/features/presentation/components/team-po-presentation-eighth-view";
+
+export function TeamPoPresentationEighthPage() {
+	return <TeamPoPresentationEighthView />;
+}
