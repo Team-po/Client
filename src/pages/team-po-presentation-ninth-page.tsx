a396776ea7affe891ea2d4e9d4bccfafdf9025b0
HwangDo <96808859+hwangjokim@users.noreply.github.com>
2026-04-29 00:42:08 +0900

    feat(presentation): add ninth team-po deck
    Closes #30

---
+++ src/pages/team-po-presentation-ninth-page.tsx
@@ -0,0 +1,5 @@
+import { TeamPoPresentationNinthView } from "@/features/presentation/components/team-po-presentation-ninth-view";
+
+export function TeamPoPresentationNinthPage() {
+	return <TeamPoPresentationNinthView />;
+}
