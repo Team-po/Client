88701c225ba3c02df12a1176a159f00828ee552c
HwangDo <96808859+hwangjokim@users.noreply.github.com>
2026-05-12 21:59:57 +0900

    feat(presentation): add team-po twelfth deck (#46)
    Closes #45

---
+++ src/pages/team-po-presentation-twelfth-page.tsx
@@ -0,0 +1,5 @@
+import { TeamPoPresentationTwelfthView } from "@/features/presentation/components/team-po-presentation-twelfth-view";
+
+export function TeamPoPresentationTwelfthPage() {
+	return <TeamPoPresentationTwelfthView />;
+}
